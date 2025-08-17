659f3a8d33db33f43d20f53930525406615d130e
Halit <xltcamm@gmail.com>
2025-08-17 10:26:17 +0200

    react-router added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    
+<Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-      </header>
-    </div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/about" element={About} />
+            
+          <Route path="/users" element={Users} />
+          
+          <Route path="/" element={Home} />
+          
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+
 export default App;
+
